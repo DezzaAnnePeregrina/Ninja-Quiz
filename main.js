99dae4aa11c3867960e7c2989019e64991fed2a6
DezzaAnnePeregrina <peregrinadezzaanne@gmail.com>
2023-06-06 12:48:22 +0800

    Codes for Ninja Quiz

---
+++ main.js
@@ -0,0 +1,40 @@
+const correctAnswers = ['B','B','B','B']
+
+const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.result')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    let score = 0
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 25
+        }
+    })
+
+
+    scrollTo(0,0)
+    result.classList.remove('d-none')
+    
+    let output = 0
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`
+        if(output === score){
+            clearInterval(timer)
+        } else{
+            output++
+        }
+    }, 10)
+})
+
+// let i = 0
+// const timer = setInterval(() => {
+//     console.log('hello')
+//     i++
+//     if(i === 5){
+//         clearInterval(timer)
+//     }
+// }, 1000)
